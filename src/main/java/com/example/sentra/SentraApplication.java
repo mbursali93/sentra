7a37f238a0ae826d90a1ea24ce8ed78945ed3014
Mustafa Bursali <m.bursali16@gmail.com>
2024-09-10 15:38:37 +0300

    first commit

---
+++ src/main/java/com/example/sentra/SentraApplication.java
@@ -0,0 +1,13 @@
+package com.example.sentra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentraApplication.class, args);
+	}
+
+}
