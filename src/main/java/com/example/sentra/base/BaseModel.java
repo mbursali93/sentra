2aa13107295bb5f90de3684b4e3de22f998a3489
mbursali <m.bursali16@gmail.com>
2024-11-23 23:39:34 +0300

    fix(user): getter annotation added for base model

--- src/main/java/com/example/sentra/base/BaseModel.java
+++ src/main/java/com/example/sentra/base/BaseModel.java
@@ -1,20 +1,20 @@
 package com.example.sentra.base;
 
 import java.time.Instant;
-import java.time.LocalDate;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 
 public class BaseModel {
     
